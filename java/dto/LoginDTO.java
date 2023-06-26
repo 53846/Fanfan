3608de5ae475040dd3227602b752aa873bc9b69b
53846 <130747086+53846@users.noreply.github.com>
2023-06-26 10:07:48 +0800

    form表单

---
+++ java/dto/LoginDTO.java
@@ -0,0 +1,37 @@
+package com.example.demo.dto;
+
+public class LoginDTO {
+    private String login_username;
+    private String login_password;
+    private String login_captcha;
+
+    public LoginDTO(String login_username, String login_password, String login_captcha) {
+        this.login_username = login_username;
+        this.login_password = login_password;
+        this.login_captcha = login_captcha;
+    }
+
+    public String getLogin_username() {
+        return login_username;
+    }
+
+    public void setLogin_username(String login_username) {
+        this.login_username = login_username;
+    }
+
+    public String getLogin_password() {
+        return login_password;
+    }
+
+    public void setLogin_password(String login_password) {
+        this.login_password = login_password;
+    }
+
+    public String getLogin_captcha() {
+        return login_captcha;
+    }
+
+    public void setLogin_captcha(String login_captcha) {
+        this.login_captcha = login_captcha;
+    }
+}
