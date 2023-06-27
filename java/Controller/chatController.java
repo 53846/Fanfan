31aa740a9913d86e0115893f8e345139a00d3c65
53846 <130747086+53846@users.noreply.github.com>
2023-06-27 09:54:56 +0800

    Update chatController.java

--- java/Controller/chatController.java
+++ java/Controller/chatController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class chatController {
 
     @GetMapping("chat")
     public String chat() {
         return "chat";
-    }}
+    }
+}
