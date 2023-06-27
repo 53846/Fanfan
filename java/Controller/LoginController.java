70d947e6d3b2ca7b2a14c3278b755632cc0d841a
53846 <130747086+53846@users.noreply.github.com>
2023-06-27 14:56:01 +0800

    Update LoginController.java

--- java/Controller/LoginController.java
+++ java/Controller/LoginController.java
@@ -2,11 +2,15 @@ package com.example.demo.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.example.demo.dto.LoginDTO;
+import com.example.demo.dto.UserDto;
+import com.example.demo.dto.UserLoginDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class LoginController {
@@ -14,5 +18,14 @@ public class LoginController {
     @GetMapping("login")
     public String login() {
         return "login";
+    }
+
+    @PostMapping("submitLogin")
+    public String submitLogin(UserLoginDto userLoginDto, HttpServletRequest request) {
+        UserDto user = new UserDto();
+        user.setUsername(userLoginDto.getLogin_username());
+        System.out.println("username: " + userLoginDto.getLogin_username() + " password: " + userLoginDto.getLogin_password() + "captcha: " + userLoginDto.getLogin_captcha());
+        request.getSession().setAttribute("user", user);
+        return "redirect:homepage";
     }
 }
