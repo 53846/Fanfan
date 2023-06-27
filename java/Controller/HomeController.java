f2289d9c2d2a0fc944bf42e41b244d6321160185
53846 <130747086+53846@users.noreply.github.com>
2023-06-27 14:55:47 +0800

    Update HomeController.java

--- java/Controller/HomeController.java
+++ java/Controller/HomeController.java
@@ -3,27 +3,20 @@ package com.example.demo.controller;
 import com.example.demo.dto.UserDto;
 import com.example.demo.dto.UserLoginDto;
 import com.example.demo.dto.UserRegisterDto;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class HomeController {
-    
+
     @GetMapping("homepage")
     public String homepage(HttpServletRequest request) {
         request.getSession().removeAttribute("msg");
         return "homepage";
     }
-    
-    @PostMapping("submitLogin")
-    public String submitLogin(UserLoginDto userLoginDto, HttpServletRequest request) {
-        UserDto user = new UserDto();
-        user.setUsername(userLoginDto.getLogin_username());
-        System.out.println("username: " + userLoginDto.getLogin_username() + " password: " + userLoginDto.getLogin_password() + "captcha: " + userLoginDto.getLogin_captcha());
-        request.getSession().setAttribute("user", user);
-        return "redirect:homepage";
-    }
+
 }
