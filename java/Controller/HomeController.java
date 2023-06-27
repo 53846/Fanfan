543b19bbe1d9da7d88daf231070b064e46b2f6e8
53846 <130747086+53846@users.noreply.github.com>
2023-06-27 09:48:02 +0800

    Update HomeController.java

--- java/Controller/HomeController.java
+++ java/Controller/HomeController.java
@@ -11,41 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
-//    @GetMapping("Index")
-//    public String index() {
-//        return "Hello World";
-//    }
-
-    @GetMapping("login")
-    public String login() {
-        return "login";
+    
+    @GetMapping("homepage")
+    public String homepage(HttpServletRequest request) {
+        request.getSession().removeAttribute("msg");
+        return "homepage";
     }
-
-    @GetMapping("content")
-    public String content() {
-        return "content";
-    }
-
-    @GetMapping("register")
-    public String register() {
-        return "register";
-    }
-
-    @GetMapping("friend")
-    public String friend() {
-        return "friend";
-    }
-
-    @GetMapping("chat")
-    public String chat() {
-        return "chat";
-    }
-
-    @GetMapping("privacyPolicy")
-    public String privacyPolicy() {
-        return "privacyPolicy";
-    }
-
+    
     @PostMapping("submitLogin")
     public String submitLogin(UserLoginDto userLoginDto, HttpServletRequest request) {
         UserDto user = new UserDto();
@@ -53,23 +25,5 @@ public class HomeController {
         System.out.println("username: " + userLoginDto.getLogin_username() + " password: " + userLoginDto.getLogin_password() + "captcha: " + userLoginDto.getLogin_captcha());
         request.getSession().setAttribute("user", user);
         return "redirect:homepage";
-    }
-
-    @PostMapping("submitRegister")
-    public String submitRegister(UserRegisterDto userRegisterDto) {
-        System.out.println("password:" + userRegisterDto.getRegister_password() + " confirmPassword:" + userRegisterDto.getRegister_confirmPassword() + " nickname:" + userRegisterDto.getRegister_nickname());
-        return "redirect:login";
-    }
-
-//    @GetMapping("index")
-//    public String index(HttpServletRequest request) {
-//        request.getSession().removeAttribute("msg");
-//        return "index";
-//    }
-
-    @GetMapping("homepage")
-    public String homepage(HttpServletRequest request) {
-        request.getSession().removeAttribute("msg");
-        return "homepage";
     }
 }
