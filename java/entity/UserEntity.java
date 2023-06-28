c5bc87a247341fc85ae6be7323df16992ab929cc
53846 <130747086+53846@users.noreply.github.com>
2023-06-28 14:06:16 +0800

    Add files via upload

---
+++ java/entity/UserEntity.java
@@ -0,0 +1,49 @@
+package com.example.demo.entity;
+
+public class UserEntity {
+    private int id;
+    private String userName;
+    private String password;
+    private String sex;
+    private String nickName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+}
