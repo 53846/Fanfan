b336a479ba5e7f9e86ac183e0bd65b5828f94e36
53846 <130747086+53846@users.noreply.github.com>
2023-06-29 09:28:12 +0800

    Add files via upload

--- java/dao/UserDao.java
+++ java/dao/UserDao.java
@@ -1,5 +1,6 @@
 package com.example.demo.dao;
 
+import com.example.demo.dto.UserModifyDto;
 import com.example.demo.entity.UserEntity;
 import org.apache.ibatis.annotations.Param;
 
@@ -7,4 +8,10 @@ import java.util.List;
 public interface UserDao {
 
     List<UserEntity> getByUserNameAndPassword(@Param(value = "userName") String userName, @Param(value = "password") String password);
+
+    void addUser(UserEntity userEntity);
+
+    UserEntity getByUserId(@Param(value = "userId") Integer userId);
+
+    void updateUser(UserModifyDto userModifyDto);
 }
