765f25e32e57ee531164d16e7f2b248a649c27af
53846 <130747086+53846@users.noreply.github.com>
2023-06-29 09:30:51 +0800

    Add files via upload

--- java/service/UserService.java
+++ java/service/UserService.java
@@ -1,7 +1,10 @@
 package com.example.demo.service;
 
 import com.example.demo.dao.UserDao;
+import com.example.demo.dto.UserModifyDto;
+import com.example.demo.dto.UserRegisterDto;
 import com.example.demo.entity.UserEntity;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
@@ -20,5 +23,20 @@ public class UserService {
             return null;
         }
         return userList.get(0);
+    }
+
+    public void addUser(UserRegisterDto userRegisterDto) {
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(userRegisterDto, userEntity);
+//        userEntity.setUserName(userRegisterDto.getUserName());
+        userDao.addUser(userEntity);
+    }
+
+    public UserEntity getByUserId(Integer userId) {
+        return userDao.getByUserId(userId);
+    }
+
+    public void updateUser(UserModifyDto userModifyDto) {
+        userDao.updateUser(userModifyDto);
     }
 }
